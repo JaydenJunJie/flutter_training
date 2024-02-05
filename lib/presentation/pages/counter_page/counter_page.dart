a7352522d1bd8d13df6dd78968ecf7b06e51ca9a
Jayden Lee <jj.lee@trinitywizards.com>
2024-02-05 11:12:07 +0800

    update the counter_page.dart and add CounterCubit.dart

--- lib/presentation/pages/counter_page/counter_page.dart
+++ lib/presentation/pages/counter_page/counter_page.dart
@@ -1,10 +1,25 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trinity_lecture_app/bloc/CounterCubit.dart';
 import 'package:trinity_lecture_app/presentation/widgets/molecules/platform_app_bar.dart';
 import 'package:trinity_lecture_app/presentation/widgets/organisms/ui_helper.dart';
 
 @RoutePage()
 class CounterPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: CounterView(),
+    );
+  }
+}
+
+class CounterView extends StatelessWidget {
+  const CounterView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,19 +27,23 @@ class CounterPage extends StatelessWidget {
         title: Text("Counter App"),
       ),
       body: Center(
-        child: Text("0"),
+        child: BlocBuilder<CounterCubit, int>(
+          builder: (BuildContext context, int state) {
+            return Text('$state');
+          },
+        ),
       ),
       floatingActionButton: Column(
         crossAxisAlignment: CrossAxisAlignment.end,
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: () => {},
+            onPressed: () => {context.read<CounterCubit>().increment()},
             child: const Icon(Icons.plus_one, semanticLabel: 'Reset chips'),
           ),
           UIHelper.verticalSpace(10),
           FloatingActionButton(
-            onPressed: () => {},
+            onPressed: () => {context.read<CounterCubit>().decrement()},
             child: const Icon(Icons.exposure_minus_1,
                 semanticLabel: 'Reset chips'),
           )
